724a5eabfb2f64a44e8aa18a7953d225b442fb32
_Sqyd_ <138494523+Sqydev@users.noreply.github.com>
2025-12-04 09:53:16 +0100

    Made CharMerge use memcpy instead pf strcpy becouse it just works better f.e. unlike with strcpy it works with \0

--- src/utils.c
+++ src/utils.c
@@ -4,11 +4,14 @@
 
 
 char* CharMerge(const char* first, const char* sec) {
+    size_t first_len = strlen(first);
+    size_t sec_len = strlen(sec);
+    
 	// TODO: Malloc err handling
-	char* res = malloc((strlen(first) + strlen(sec) + 1) * sizeof(char));
+	char* res = malloc((first_len + sec_len + 1) * sizeof(char));
 
-	strcpy(res, first);
-    strcat(res, sec);
+    memcpy(res, first, first_len);
+    memcpy(res + first_len, sec, sec_len);
 
 	return res;
 }
